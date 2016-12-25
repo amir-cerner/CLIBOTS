ab77e6eeaa286292d9e70eb77ccedfeca05a6fb8
telecerner <amircerner@gmail.com>
2016-12-25 19:28:30 +0330

    Update steady.sh

--- steady.sh
+++ steady.sh
@@ -299,9 +299,9 @@ sleep 0.5
 
 echo ""
 echo -e "\033[38;5;208m 𝓟𝓸𝔀𝒆𝓻𝒆𝓭 𝓫𝔂: \033[0;00m"
-echo -e "\033[38;5;208m Ａｄｖａｎ Ｔｅａｍ \033[0;00m"
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
+echo -e "\033[38;5;208m 🇨 🇪 🇷 🇳 🇪 🇷  🇹 🇪 🇦 🇲  \033[0;00m"
+echo -e "\033[38;5;208m @cernerteam    :)      @cruel_channel \033[0;00m"
+echo -e "\033[38;5;208m @cernerteam    :)      @cruel_channel \033[0;00m"
 # Starting preliminar setup
 sleep 1.5
 echo -e "$bld$f4 CHECKING PROCESSES...$rst"
@@ -568,9 +568,9 @@ while getopts ":tsTSih" opt; do
 	echo -e "\e[0m"
 
 echo -e "\033[38;5;208m 𝓟𝓸𝔀𝒆𝓻𝒆𝓭 𝓫𝔂: \033[0;00m"
-echo -e "\033[38;5;208m Ａｄｖａｎ Ｔｅａｍ \033[0;00m"
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
+echo -e "\033[38;5;208m 🇨 🇪 🇷 🇳 🇪 🇷  🇹 🇪 🇦 🇲  \033[0;00m"
+echo -e "\033[38;5;208m @cernerteam    :)      @cernerteam \033[0;00m"
+echo -e "\033[38;5;208m @cernerteam    :)      @cernerteam \033[0;00m"
 echo ""
 	exit 1
       ;;
